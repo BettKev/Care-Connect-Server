3707ea29d874f1fb8aa42595c65477fdb3523a61
Kevin Bett <prodigy4614@gmail.com>
2024-10-16 14:05:06 +0300

    Fix json server deployment issue

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-// This section handles the user registration form it takes tttthe values entered in the form and passes them to the server
-const userRegistrationForm = document.querySelector('form#userForm');
-userRegistrationForm.addEventListener("submit", (event)=>{
-  event.preventDefault();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({ static: '' }); // Disable static files
 
-  const firstName = document.getElementById('firstName').value;
-  const lastName = document.getElementById('lastName').value;
-  const idNumber = document.getElementById('idNumber').value;
-  const countryOfBirth = document.getElementById('countryOfBirth').value;
-  const countyOfBirth = document.getElementById('countyOfBirth').value;
-  const disabilityType = document.getElementById('disabilityType').value;
-  const coordinates = document.getElementById('coordinates').value;
-  const photoLink = document.getElementById('photoLink').value;
-  const comments = document.getElementById('comments').value;
-
-  fetch('http://localhost:3000/users', {
-    method: 'POST',
-  body: JSON.stringify({
-    firstName: firstName,
-    lastName: lastName,
-    idNumber: idNumber,
-    countryOfBirth: countryOfBirth,
-    countyOfBirth: countyOfBirth,
-    disabilityType: disabilityType,
-    coordinates: coordinates,
-    photoLink: photoLink,
-    comments: comments
-
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-  })
-  .then((response) => response.json())
-        .then((res) => {
-          const message = document.getElementById("message");
-          message.innerText = "User Successfully registered";
-          
-        });
-})
+server.use(middlewares);
+server.use(router);
+server.listen(8080, () => {
+  console.log('JSON Server is running on port 8080');
+});
